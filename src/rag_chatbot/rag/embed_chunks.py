77061ce61d31506f8ab0e816eaa19230ddd45637
Alexiboi <alexhanna413@gmail.com>
2026-03-24 17:46:23 +0000

    Change main.py to be methods for exclusively chunking and embedding chunks and rename to embed_chunks.py

---
+++ src/rag_chatbot/rag/embed_chunks.py
@@ -0,0 +1,35 @@
+
+import asyncio
+
+from src.rag_chatbot.rag.blob_utils import chunk_from_blob
+from src.rag_chatbot.rag.embedding_utils import process_and_store_chunks
+from src.rag_chatbot.rag.RAG_bot import chat_loop
+from src.rag_chatbot.rag.env import transcript_container_client, notes_container_client
+
+async def embed_chunks():
+   
+    choice = int(input("1: chunk transcripts only\n2: chunk meeting notes only\n3. Chunk both/all blobs\n"))
+    transcript_chunks = []
+    meeting_note_chunks = []
+    if choice == 1:
+        transcript_chunks = chunk_from_blob(
+            transcript_container_client,
+            doc_type="transcript",
+            chunk_size=756
+        )
+    elif choice == 2:
+        meeting_note_chunks = chunk_from_blob(
+            notes_container_client,
+            doc_type="meeting_note",
+            chunk_size=400, # smaller may be better for structured note
+            epic_chunking=True,
+            overlap=True # test context chunking for meeting notes
+        )
+    else:
+        print("Invalid choice!")
+
+    all_chunks = transcript_chunks + meeting_note_chunks
+    process_and_store_chunks(all_chunks)    
+
+if __name__ == '__main__':
+    asyncio.run(main())
