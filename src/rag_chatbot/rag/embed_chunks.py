f6c2fa8ee5f597708af0190e7264502b897a4b7c
Alexiboi <alexhanna413@gmail.com>
2026-03-26 14:57:48 +0000

    Comment embed_chunks method

--- src/rag_chatbot/rag/embed_chunks.py
+++ src/rag_chatbot/rag/embed_chunks.py
@@ -1,13 +1,17 @@
 
 import asyncio
-
 from src.rag_chatbot.rag.blob_utils import chunk_from_blob
 from src.rag_chatbot.rag.embedding_utils import process_and_store_chunks
-from src.rag_chatbot.rag.RAG_bot import chat_loop
 from src.rag_chatbot.rag.env import transcript_container_client, notes_container_client
 
 async def embed_chunks():
-   
+    """
+    method designed to be run from the command line. First chunks all the blobs from a specific container, user can chose to chunk
+    all the blob's from the transcript container or meeting container or both.
+
+    Then chunks are stored in their corresponding azure AI search index. So transcript_container blobs are stored in the transcripts
+    container for example.
+    """
     choice = int(input("1: chunk transcripts only\n2: chunk meeting notes only\n3. Chunk both/all blobs\n"))
     transcript_chunks = []
     meeting_note_chunks = []
@@ -32,4 +36,4 @@ async def embed_chunks():
     process_and_store_chunks(all_chunks)    
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    asyncio.run(embed_chunks())
